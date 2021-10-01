01e54422d8016c298d0e185b4e5b438390a64bca
LeonardoRamazzotti <leonardo.ramazzotti95@gmail.com>
2021-10-01 18:13:26 +0200

    update

--- main.py
+++ main.py
@@ -1,21 +1,66 @@
 # Deep Writing , a based Python Software for deep focus in writing without distraction
 
 from tkinter import *
+from configparser import ConfigParser
+from tkinter import PhotoImage
+
 # COLOR PALETTE =============================================================================================================================
 
 Dark_grey = '#1E1E1E'
 White_sheet = '#FFFFFB'
 Blu_navy = '#304D63'
+grey = '#424242'
 
 
 #END  COLOR PALETTE =============================================================================================================================
 
+# SETTINGS ======================================================================================================================================
+parser = ConfigParser()
+parser.read('config.txt')
+saved_sheet_color = parser.get('color','main_color')
+saved_bg_color = parser.get('color', 'background')
+saved_font_color =parser.get('color','font_color' )
+
+
+#END SETTINGS ======================================================================================================================================
+
+def fullscreen():
+    
+    root.attributes('-fullscreen',True) 
+
+
+def resize():
+
+    root.attributes('-fullscreen',False)     
+
+
+
+screen_check = False
+
 root = Tk()
 root.title('Deep Writing')
-root.attributes('-fullscreen',False)
+
+#IMAGE SECTION =====================================================================================================================================
+
+quit_bt = PhotoImage(file='quit.png')
+
+#END IMAGE SECTION =================================================================================================================================
+
 screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 win_size = str(screen_width)+'x'+str(screen_height-70)
 root.geometry(win_size)
+
+root.config(bg=saved_bg_color)
+root.attributes('-fullscreen',True) 
+
+frame_page = Frame(root,bg = saved_sheet_color,width=800,height=1040, borderwidth=0,highlightthickness=0)
+frame_page.place(x=560,y=20)
+
+text_area = Text(root,bg = saved_sheet_color,fg=saved_font_color,width=75,height=62,  borderwidth=0,highlightthickness=0)
+text_area.place(x=660,y=60)
+quit_button = Button(root,image=quit_bt, borderwidth=0,highlightthickness=0,bg=saved_bg_color,activebackground=saved_bg_color,command = lambda: root.destroy())
+quit_button.place(x=10,y=10)
+
 
 root.mainloop()
